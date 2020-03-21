1a52b398bedf92152cbea3fef57d17226d5b0f54
mateja <mateja176@gmail.com>
2020-03-22 00:12:58 +0100

    feat(views): adds detail and results

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Choice, Question
 
@@ -7,3 +8,20 @@ def index(request):
     latest_questions = Question.objects.order_by('-pub_date')
     context = {'latest_questions': latest_questions}
     return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/results.html', {question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/results.html', {question})
